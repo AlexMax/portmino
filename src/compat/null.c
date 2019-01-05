f3e1175026d4ae5ac9727de5c44785fbd3f62ddc
Alex Mayfield <alexmax2742@gmail.com>
2019-01-05 17:51:55 -0500

    MinGW build fixups

---
+++ src/compat/null.c
@@ -0,0 +1,7 @@
+/**
+ * This file intentionally left blank
+ * 
+ * In alls seriousness, this file is needed just in case no compatibility
+ * shims are used, to ensure that portmino-compat target consists of one
+ * valid TU.
+ */
